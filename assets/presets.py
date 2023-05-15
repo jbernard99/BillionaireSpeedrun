0e36559430f2ac145c4861eeb1b533420862ec28
Julien Bernard <jbernard@c2r4p6.42quebec.com>
2023-05-15 10:30:33 -0400

    Added platform based fonts

--- assets/presets.py
+++ assets/presets.py
@@ -1,4 +1,5 @@
 import pygame
+import platform
 
 WHITE 	= (255, 255, 255)
 BLACK 	= (0, 0, 0)
@@ -7,9 +8,14 @@ GREEN	= (0, 255, 0)
 BLUE	= (0, 0, 255)
 GREY	= (128, 128, 128)
 
-SMALL_D_FONT = pygame.font.SysFont("urwbookman", 20)
-MEDIUM_D_FONT = pygame.font.SysFont("urwbookman", 24)
-BIG_D_FONT = pygame.font.SysFont("urwbookman", 32)
+if platform.system() == "Darwin":
+	SMALL_D_FONT = pygame.font.SysFont("hiraginosansgb", 18)
+	MEDIUM_D_FONT = pygame.font.SysFont("hiraginosansgb", 22)
+	BIG_D_FONT = pygame.font.SysFont("hiraginosansgb", 32)
+elif platform.system() == "Linux":
+	SMALL_D_FONT = pygame.font.SysFont("urwbookman", 20)
+	MEDIUM_D_FONT = pygame.font.SysFont("urwbookman", 24)
+	BIG_D_FONT = pygame.font.SysFont("urwbookman", 32)
 
 GEEKSQUAD 	= "Work at GeekSquad(Get $$$)"
 EBOOK 		= "Write and ebook using ChatGPT"
